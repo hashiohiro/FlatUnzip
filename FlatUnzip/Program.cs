a5edff602753666b3c043ca5db4a1c5cf3fde0e4
橋本　浩史 <HashimotoHiroshi@sgis.co.jp>
2022-02-22 19:22:52 +0900

    初期投入

---
+++ FlatUnzip/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO.Compression;
+
+if (args.Length != 2)
+{
+    Console.WriteLine("Usage : FlatUnzip.exe [SrcFolder] [DistFolder]");
+    Environment.Exit(1);
+}
+
+var src = new DirectoryInfo(args[0]);
+var dist = new DirectoryInfo(args[1]);
+if (!dist.Exists)
+{
+    dist.Create();
+}
+
+var zipFiles = src.GetFiles("*.zip", SearchOption.AllDirectories);
+
+foreach (var each in zipFiles)
+    ZipFile.ExtractToDirectory(each.FullName, dist.FullName);
